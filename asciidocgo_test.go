7b680c07ac57d9d6484979301f6f395ede0c36c8
VonC <vonc@laposte.net>
2013-12-13 17:52:56 +0100

    Add input entry test declaration for Asciidocgo.

--- asciidocgo_test.go
+++ asciidocgo_test.go
@@ -5,7 +5,11 @@ import . "github.com/smartystreets/goconvey/convey"
 
 func TestAsciidocgo(t *testing.T) {
 	Load(nil)
-	Convey("Asciidocgo takes a Reader and return a Document", t, func() {
+	Convey("Asciidocgo load() takes a string and return a Document", t, nil)
+	Convey("Asciidocgo load() takes a array and return a Document", t, nil)
+	Convey("Asciidocgo load() takes a IO and return a Document", t, nil)
+
+	Convey("Asciidocgo load() takes a Reader and return a Document", t, func() {
 		Convey("A nil Reader must returns a nil Document", func() {
 			So(Load(nil), ShouldBeNil)
 		})
