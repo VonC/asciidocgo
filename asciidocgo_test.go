32e34d60e780d2e9b91fcc3f27d9bf4c3bbf217f
VonC <vonc@laposte.net>
2013-12-13 10:51:42 +0100

    First Convey Test, for nil Reader.

--- asciidocgo_test.go
+++ asciidocgo_test.go
@@ -1,7 +1,13 @@
 package asciidocgo
 
 import "testing"
+import . "github.com/smartystreets/goconvey/convey"
 
 func TestAsciidocgo(t *testing.T) {
-	load(nil)
+	Load(nil)
+	Convey("Asciidocgo takes a Reader and return a Document", t, func() {
+		Convey("A nil Reader must returns a nil Document", func() {
+			So(Load(nil), ShouldBeNil)
+		})
+	})
 }
