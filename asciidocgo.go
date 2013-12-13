51b04144d7a863cf32fff51769664ad09a915a4e
VonC <vonc@laposte.net>
2013-12-13 10:59:51 +0100

    Add Package doc, and Lod function doc.

--- asciidocgo.go
+++ asciidocgo.go
@@ -1,7 +1,13 @@
+/* Asciidocgo implements an AsciiDoc renderer in Go. */
 package asciidocgo
 
 import "io"
 
+/*
+Accepts input as an IO (or StringIO), String or String Array object.
+If the input is a File, information about the file is stored in attributes on
+the Document object.
+*/
 func Load(input io.Reader) *Document {
 	return nil
 }
