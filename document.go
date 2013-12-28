524698cb745d745c3f33ce9cad89de0e1739b40e
VonC <vonc@laposte.net>
2013-12-28 15:05:00 +0100

    Add document ReadTime, and its test

--- document.go
+++ document.go
@@ -1,11 +1,15 @@
 package asciidocgo
 
+import "errors"
+
 // Asciidoc Document, onced loaded from an IO, string or array
 type Document struct {
 	monitorData *monitorData
 }
 
-type monitorData struct{}
+type monitorData struct {
+	readTime int
+}
 
 // Check if a Document is supposed to be monitored
 func (d *Document) IsMonitored() bool {
@@ -20,4 +24,11 @@ func (d *Document) Monitor() *Document {
 		d.monitorData = new(monitorData)
 	}
 	return d
+}
+
+func (d *Document) ReadTime() (readTime int, err error) {
+	if d.IsMonitored() == false {
+		return 0, errors.New("z")
+	}
+	return d.monitorData.readTime, nil
 }
--- document_test.go
+++ document_test.go
@@ -5,13 +5,20 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestDocument(t *testing.T) {
+var dm = new(Document).Monitor()
+var dnm = new(Document)
+
+func TestDocumentMonitor(t *testing.T) {
 	Convey("A Document can be monitored", t, func() {
 		Convey("By default, a Document is not monitored", func() {
-			So(new(Document).IsMonitored(), ShouldBeFalse)
+			So(dnm.IsMonitored(), ShouldBeFalse)
 		})
 		Convey("A monitored Document is monitored", func() {
-			So(new(Document).Monitor().IsMonitored(), ShouldBeTrue)
+			So(dm.IsMonitored(), ShouldBeTrue)
 		})
+	})
+	Convey("A non-monitored Document should return error when accessing times", t, func() {
+		_, err := dnm.ReadTime()
+		So(err, ShouldNotBeNil)
 	})
 }
