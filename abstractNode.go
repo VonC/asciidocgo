abe0ba46f097640f1db63d14e80ffae39fe7d6ea
VonC <vonc@laposte.net>
2013-12-30 22:48:20 +0100

    abstractNode with document context: document is parent.

--- abstractNode.go
+++ abstractNode.go
@@ -5,9 +5,31 @@ a node of AsciiDoc content.
 The state and methods on this class are comment to all content segments
 in an AsciiDoc document. */
 type abstractNode struct {
+	parent   *abstractNode
+	context  context
+	document *abstractNode
 }
 
 func newAbstractNode(parent *abstractNode, context context) *abstractNode {
-	abstractNode := &abstractNode{}
+	abstractNode := &abstractNode{parent, context, nil}
+	if context == document {
+		abstractNode.parent = nil
+		abstractNode.document = parent
+	}
 	return abstractNode
+}
+
+//  Get the element which is the parent of this node
+func (an *abstractNode) Parent() *abstractNode {
+	return an.parent
+}
+
+//  Get the Asciidoctor::Document to which this node belongs
+func (an *abstractNode) Document() *abstractNode {
+	return an.document
+}
+
+// Get the Symbol context for this node
+func (an *abstractNode) Context() context {
+	return an.context
 }
--- abstractNode_test.go
+++ abstractNode_test.go
@@ -15,8 +15,12 @@ func TestAbstractNode(t *testing.T) {
 		Convey("An AbstractNode takes a parent and a context", func() {
 			So(newAbstractNode(nil, document), ShouldNotBeNil)
 		})
-		Convey("If context is a document, then parent is nil even if param 'parent' was not", func() {
-			So(newAbstractNode(&abstractNode{}, document).Parent(), ShouldBeNil)
+		Convey("If context is a document, then parent is nil and document is parent", func() {
+			parent := &abstractNode{}
+			an := newAbstractNode(parent, document)
+			So(an.Context(), ShouldEqual, document)
+			So(an.Parent(), ShouldBeNil)
+			So(an.Document(), ShouldEqual, parent)
 		})
 	})
 }
