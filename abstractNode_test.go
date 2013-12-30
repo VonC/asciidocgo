cb5fd09653f5c72cf27fbf6b27cd7331f40b2b7f
VonC <vonc@laposte.net>
2013-12-30 22:31:27 +0100

    An abstractNode with document context has a nil parent.

--- abstractNode_test.go
+++ abstractNode_test.go
@@ -15,5 +15,8 @@ func TestAbstractNode(t *testing.T) {
 		Convey("An AbstractNode takes a parent and a context", func() {
 			So(newAbstractNode(nil, document), ShouldNotBeNil)
 		})
+		Convey("If context is a document, then parent is nil even if param 'parent' was not", func() {
+			So(newAbstractNode(&abstractNode{}, document).Parent(), ShouldBeNil)
+		})
 	})
 }
