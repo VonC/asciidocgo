4d91ce0d44e26b618e9dbd1d36175c9867b3795d
VonC <vonc@laposte.net>
2014-01-17 16:34:59 +0100

    SystemPath: non-empty targetis appended to non-empty start.

--- pathResolver_test.go
+++ pathResolver_test.go
@@ -286,11 +286,14 @@ func TestPathResolver(t *testing.T) {
 			So(pr.SystemPath("C:/etc/images", "", "", false, ""), ShouldEqual, "C:/etc/images")
 		})
 
+		Convey("non-empty targetis appended to non-empty start", func() {
+			// resolver.system_path('images', '/etc')
+			// => '/etc/images'
+			So(pr.SystemPath("images", "C:/etc", "", false, ""), ShouldEqual, "C:/etc/images2s")
+		})
 	})
 	/*
 
-	   resolver.system_path('images', '/etc')
-	   => '/etc/images'
 
 	   resolver.system_path('', '/etc/images')
 	   => '/etc/images'
