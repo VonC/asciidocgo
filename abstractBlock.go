89bef59bb0a90bf5af541e7d666c29701af842f7
VonC <vonc@laposte.net>
2014-01-25 23:15:38 +0100

    Add subs, and templateName (based on context string) on abstractBlock.

--- abstractBlock.go
+++ abstractBlock.go
@@ -5,9 +5,12 @@ a block of AsciiDoc content, which is a node. */
 type abstractBlock struct {
 	*abstractNode
 	contentModel
+	subs         []string
+	templateName string
 }
 
 func newAbstractBlock(parent Documentable, context context) *abstractBlock {
-	abstractBlock := &abstractBlock{newAbstractNode(parent, context), compound}
+	templateName := "block_" + context.String()
+	abstractBlock := &abstractBlock{newAbstractNode(parent, context), compound, []string{}, templateName}
 	return abstractBlock
 }
