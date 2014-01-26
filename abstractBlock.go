4f1b8b7d071d09974b7b2f8723c75f3959e9ae0f
VonC <vonc@laposte.net>
2014-01-26 09:07:22 +0100

    Test accessors for abstractBlock.

--- abstractBlock.go
+++ abstractBlock.go
@@ -15,3 +15,29 @@ func newAbstractBlock(parent Documentable, context context) *abstractBlock {
 	abstractBlock := &abstractBlock{newAbstractNode(parent, context), compound, []string{}, templateName, []*abstractBlock{}}
 	return abstractBlock
 }
+
+/* The types of content that this block can accomodate */
+func (ab *abstractBlock) ContentModel() contentModel {
+	return ab.contentModel
+}
+func (ab *abstractBlock) SetContentModel(c contentModel) {
+	ab.contentModel = c
+}
+
+/* Substitutions to be applied to content in this block */
+func (ab *abstractBlock) Subs() []string {
+	return ab.subs
+}
+
+/* Get/Set the String name of the render template */
+func (ab *abstractBlock) TemplateName() string {
+	return ab.templateName
+}
+func (ab *abstractBlock) SetTemplateName(tn string) {
+	ab.templateName = tn
+}
+
+/* Array of Asciidoctor::AbstractBlock sub-blocks for this block */
+func (ab *abstractBlock) Blocks() []*abstractBlock {
+	return ab.blocks
+}
--- abstractBlock_test.go
+++ abstractBlock_test.go
@@ -7,13 +7,28 @@ import (
 
 func TestAbstractBlock(t *testing.T) {
 
-	Convey("An abstractNode can be initialized", t, func() {
-
+	Convey("An abstractBlock can be initialized", t, func() {
+		ab := newAbstractBlock(nil, document)
 		Convey("By default, an AbstractBlock can be created", func() {
 			So(&abstractBlock{}, ShouldNotBeNil)
-			So(newAbstractBlock(nil, document), ShouldNotBeNil)
+			So(ab, ShouldNotBeNil)
 		})
-
+		Convey("By default, an AbstractBlock has a 'compound' content model", func() {
+			So(ab.ContentModel(), ShouldEqual, compound)
+			ab.SetContentModel(simple)
+			So(ab.ContentModel(), ShouldEqual, simple)
+		})
+		Convey("By default, an AbstractBlock has no subs", func() {
+			So(len(ab.Subs()), ShouldEqual, 0)
+		})
+		Convey("By default, an AbstractBlock has a template name equals to block_#{context}", func() {
+			So(ab.TemplateName(), ShouldEqual, "block_"+ab.Context().String())
+			ab.SetTemplateName("aTemplateName")
+			So(ab.TemplateName(), ShouldEqual, "aTemplateName")
+		})
+		Convey("By default, an AbstractBlock has no blocks", func() {
+			So(len(ab.Blocks()), ShouldEqual, 0)
+		})
 	})
 
 }
