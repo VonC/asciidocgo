b408968713566a2d4af8bb0840f91fd27a87d4b1
VonC <vonc@laposte.net>
2014-02-12 13:52:53 +0100

    Fully test regexps.Reres.

--- consts/regexps/regexps.go
+++ consts/regexps/regexps.go
@@ -28,6 +28,8 @@ var ORDERED_LIST_KEYWORDS = map[string]rune{
 	//'decimal': '1'
 }
 
+/* Encapsulate a regex and a string,
+for managing results from FindAllStringSubmatchIndex */
 type Reres struct {
 	r        *regexp.Regexp
 	s        string
@@ -36,27 +38,79 @@ type Reres struct {
 	previous int
 }
 
+/* Build new result from FindAllStringSubmatchIndex on a string */
 func NewReres(s string, r *regexp.Regexp) *Reres {
 	matches := r.FindAllStringSubmatchIndex(s, -1)
 	return &Reres{r, s, matches, 0, 0}
 }
 
+/* Check if there is any match */
 func (rr *Reres) HasAnyMatch() bool {
 	return len(rr.matches) > 0
 }
 
+/* Check if there is one more match */
 func (rr *Reres) HasNext() bool {
 	return rr.i < len(rr.matches)
 }
 
+/* Refers to the next match, for Group() to works with */
 func (rr *Reres) Next() {
 	rr.previous = rr.matches[rr.i][1]
 	rr.i = rr.i + 1
 }
 
-func (rr *Reres) Previous() string {
+/* Get back to the first match */
+func (rr *Reres) ResetNext() {
+	rr.i = 0
+	rr.previous = 0
+}
+
+/* String from the last match to current one */
+func (rr *Reres) Prefix() string {
 	mi := rr.matches[rr.i]
 	return rr.s[rr.previous:mi[0]]
+}
+
+/* String from current match to the end ofthe all string */
+func (rr *Reres) Suffix() string {
+	return rr.s[rr.previous:]
+}
+
+/* First character of the current match */
+func (rr *Reres) FirstChar() uint8 {
+	mi := rr.matches[rr.i]
+	return rr.s[mi[0]]
+}
+
+/* Full string matched for the current group */
+func (rr *Reres) FullMatch() string {
+	mi := rr.matches[rr.i]
+	return rr.s[mi[0]:mi[1]]
+}
+
+/* Check if the ith group if present in the current match */
+func (rr *Reres) HasGroup(j int) bool {
+	res := false
+	mi := rr.matches[rr.i]
+	if len(mi) > (j*2)+1 {
+		if mi[j*2] > -1 {
+			if mi[j*2] < mi[(j*2)+1] {
+				res = true
+			}
+		}
+	}
+	return res
+}
+
+/* return the ith group string, if present in the current match */
+func (rr *Reres) Group(i int) string {
+	res := ""
+	if rr.HasGroup(i) {
+		mi := rr.matches[rr.i]
+		res = rr.s[mi[i*2]:mi[(i*2)+1]]
+	}
+	return res
 }
 
 /* The following pattern, which appears frequently, captures the contents
--- consts/regexps/regexps_test.go
+++ consts/regexps/regexps_test.go
@@ -1,6 +1,7 @@
 package regexps
 
 import (
+	"regexp"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -38,4 +39,84 @@ func TestRegexps(t *testing.T) {
 		So(EscapedBracketRx.MatchString(`\]`), ShouldBeTrue)
 		So(EscapedBracketRx.MatchString(`a\\]a`), ShouldBeTrue)
 	})
+
+	Convey("Regexps can encapsulate results in a struct Reres", t, func() {
+		testRx, _ := regexp.Compile("a(b*)c")
+		r := NewReres("xxxabbbbcyyy111aabbbcc222ac33", testRx)
+
+		Convey("Regexps can create a Reres struct", func() {
+			So(r, ShouldNotBeNil)
+		})
+		Convey("Regexps can test for matches", func() {
+			So(r.HasAnyMatch(), ShouldBeTrue)
+		})
+
+		Convey("Regexps can iterate over matches", func() {
+			So(r.HasNext(), ShouldBeTrue)
+			r.Next()
+			So(r.HasNext(), ShouldBeTrue)
+			r.Next()
+			So(r.HasNext(), ShouldBeTrue)
+			r.Next()
+			So(r.HasNext(), ShouldBeFalse)
+			r.ResetNext()
+		})
+
+		Convey("Regexps can get the prefix, string before each match", func() {
+			So(r.Prefix(), ShouldEqual, "xxx")
+			r.Next()
+			So(r.Prefix(), ShouldEqual, "yyy111a")
+			r.ResetNext()
+		})
+
+		Convey("Regexps can get the suffix, string after each match", func() {
+			So(r.Suffix(), ShouldEqual, "xxxabbbbcyyy111aabbbcc222ac33")
+			r.Next()
+			So(r.Suffix(), ShouldEqual, "yyy111aabbbcc222ac33")
+			r.Next()
+			So(r.Suffix(), ShouldEqual, "c222ac33")
+			r.Next()
+			So(r.Suffix(), ShouldEqual, "33")
+			r.ResetNext()
+		})
+
+		Convey("Regexps can get the first character of the current match", func() {
+			So(r.FirstChar(), ShouldEqual, 'a')
+		})
+
+		Convey("Regexps can get the full string of the current match", func() {
+			So(r.FullMatch(), ShouldEqual, "abbbbc")
+			r.Next()
+			So(r.FullMatch(), ShouldEqual, "abbbc")
+			r.Next()
+			So(r.FullMatch(), ShouldEqual, "ac")
+			r.ResetNext()
+		})
+
+		Convey("Regexps can test for a group within the current match", func() {
+			//fmt.Printf("t %v (%d) %v => %d\n", r.matches, r.i, r.matches[r.i], r.previous)
+			So(r.HasGroup(1), ShouldBeTrue)
+			So(r.HasGroup(2), ShouldBeFalse)
+			r.Next()
+			So(r.HasGroup(1), ShouldBeTrue)
+			So(r.HasGroup(2), ShouldBeFalse)
+			r.Next()
+			So(r.HasGroup(1), ShouldBeFalse)
+			So(r.HasGroup(2), ShouldBeFalse)
+			r.ResetNext()
+		})
+
+		Convey("Regexps can get a group within the current match", func() {
+			So(r.Group(1), ShouldEqual, "bbbb")
+			So(r.Group(2), ShouldEqual, "")
+			r.Next()
+			So(r.Group(1), ShouldEqual, "bbb")
+			So(r.Group(2), ShouldEqual, "")
+			r.Next()
+			So(r.Group(1), ShouldEqual, "")
+			So(r.Group(2), ShouldEqual, "")
+			r.ResetNext()
+		})
+	})
+
 }
