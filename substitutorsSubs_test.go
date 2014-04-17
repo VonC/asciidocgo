8a6222212f0c8e1ff1aef1d928621816617fc7aa
VonC <vonc@laposte.net>
2014-04-17 11:06:06 +0200

    Complete test for aTo subs.

--- substitutorsSubs_test.go
+++ substitutorsSubs_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSubstitutorSubs(t *testing.T) {
 
-	Convey("A sunEnum can be converted from string", t, func() {
+	Convey("A subEnum can be converted from string", t, func() {
 		So(aToSE(string(subsBasic)), ShouldEqual, sub.basic)
 		So(aToSE(string(subsNormal)), ShouldEqual, sub.normal)
 		So(aToSE(string(subsVerbatim)), ShouldEqual, sub.verbatim)
@@ -17,5 +17,38 @@ func TestSubstitutorSubs(t *testing.T) {
 		So(aToSE(string(subsPass)), ShouldEqual, sub.pass)
 		So(aToSE(string(subsUnknown)), ShouldEqual, sub.unknown)
 		So(aToSE("xxxtestxxx"), ShouldEqual, nil)
+	})
+	Convey("A subValue can be converted from string", t, func() {
+		So(aToSEValues(string(subsSpecialCharacters)), ShouldEqual, subValue.specialcharacters)
+		So(aToSEValues(string(subsQuotes)), ShouldEqual, subValue.quotes)
+		So(aToSEValues(string(subsAttributes)), ShouldEqual, subValue.attributes)
+		So(aToSEValues(string(subsReplacements)), ShouldEqual, subValue.replacements)
+		So(aToSEValues(string(subsMacros)), ShouldEqual, subValue.macros)
+		So(aToSEValues(string(subsPostReplacements)), ShouldEqual, subValue.replacements)
+		So(aToSEValues(string(subsCallout)), ShouldEqual, subValue.callouts)
+		So(aToSEValues("xxxtestxxx1"), ShouldEqual, nil)
+	})
+	Convey("A composite SE can be converted from string", t, func() {
+		So(aToCompositeSE(string(subsNone)), ShouldEqual, compositeSub.none)
+		So(aToCompositeSE(string(subsNormal)), ShouldEqual, compositeSub.normal)
+		So(aToCompositeSE(string(subsVerbatim)), ShouldEqual, compositeSub.verbatim)
+		So(aToCompositeSE(string(subsSpecialChars)), ShouldEqual, compositeSub.specialchars)
+		So(aToCompositeSE("xxxtestxxx2"), ShouldEqual, nil)
+	})
+	Convey("A subSymbol can be converted from string", t, func() {
+		So(aToSubSymbol(string(subsA)), ShouldEqual, subSymbol.a)
+		So(aToSubSymbol(string(subsM)), ShouldEqual, subSymbol.m)
+		So(aToSubSymbol(string(subsN)), ShouldEqual, subSymbol.n)
+		So(aToSubSymbol(string(subsP)), ShouldEqual, subSymbol.p)
+		So(aToSubSymbol(string(subsQ)), ShouldEqual, subSymbol.q)
+		So(aToSubSymbol(string(subsR)), ShouldEqual, subSymbol.r)
+		So(aToSubSymbol(string(subsC)), ShouldEqual, subSymbol.c)
+		So(aToSubSymbol(string(subsV)), ShouldEqual, subSymbol.v)
+		So(aToSubSymbol("xxxtestxxx3"), ShouldEqual, nil)
+	})
+	Convey("A subOption can be converted from string", t, func() {
+		So(aToSubOption(string(subsBlock)), ShouldEqual, subOption.block)
+		So(aToSubOption(string(subsInline)), ShouldEqual, subOption.inline)
+		So(aToCompositeSE("xxxtestxxx4"), ShouldEqual, nil)
 	})
 }
